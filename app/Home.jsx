24538e91fb586a736bfe5dd7e92b125a7255d271
SHUBHAM GUPTA <gshubham804@gmail.com>
2023-07-05 16:37:01 +0530

    project started

---
+++ app/Home.jsx
@@ -0,0 +1,24 @@
+// pages/index.js
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Homepage</title>
+        <meta name="description" content="Welcome to my homepage" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to My Homepage</h1>
+        <p>Feel free to explore!</p>
+      </main>
+
+      <footer>
+        <p>© 2023 My Homepage. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
